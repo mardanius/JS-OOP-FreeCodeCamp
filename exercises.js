0bc9392dcc2265206be0dfd10cc5a73dfc4431af
mardanius <mariojeda.mg@gmail.com>
2022-05-29 13:51:24 -0400

    Set the Child's Prototype to an Instance of the Parent'

--- exercises.js
+++ exercises.js
@@ -649,21 +649,25 @@ function Animal() { }
 Animal.prototype.eat = function() {
   console.log("nom nom nom");
 };
+
 This and the next challenge will cover how to reuse the methods of Animal inside Bird and Dog without defining them again. 
 It uses a technique called inheritance. This challenge covers the first step: make an instance of the supertype (or parent). 
 You already know one way to create an instance of Animal using the new operator:
 
 let animal = new Animal();
+
 There are some disadvantages when using this syntax for inheritance, which are too complex for the scope of this challenge. 
 Instead, here's an alternative approach without those disadvantages:
 
 let animal = Object.create(Animal.prototype);
+
 Object.create(obj) creates a new object, and sets obj as the new object's prototype. Recall that the prototype is like the "recipe" 
 for creating an object. By setting the prototype of animal to be the prototype of Animal, you are effectively giving the animal 
 instance the same "recipe" as any other instance of Animal.
 
 animal.eat();
 animal instanceof Animal;
+
 The instanceof method here would return true.
 -----------------------------------------------------------------------
 Use Object.create to make two instances of Animal named duck and beagle. */
@@ -681,5 +685,41 @@ Animal.prototype = {
 
 let duck = Object.create(Animal.prototype); // Change this line
 let beagle = Object.create(Animal.prototype); // Change this line
+
+
+
+/*----------------------------------------------------
+Set the Child's Prototype to an Instance of the Parent
+------------------------------------------------------
+In the previous challenge you saw the first step for inheriting behavior from the supertype (or parent) Animal: making a new instance of Animal.
+
+This challenge covers the next step: set the prototype of the subtype (or child)—in this case, Bird—to be an instance of Animal.
+
+Bird.prototype = Object.create(Animal.prototype);
+Remember that the prototype is like the "recipe" for creating an object. In a way, the recipe for Bird now includes all the key "ingredients" 
+from Animal.
+
+let duck = new Bird("Donald");
+duck.eat();
+
+duck inherits all of Animal's properties, including the eat method.
+--------------------------------------------------------------
+Modify the code so that instances of Dog inherit from Animal.*/
+
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Dog() { }
+
+// Only change code below this line
+
+Dog.prototype = Object.create(Animal.prototype)
+let beagle = new Dog();
 
 
